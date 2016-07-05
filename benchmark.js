785415b58f92f033559adf1ee779c8663c37c2c6
Gabriel Aumala <gabrielfima@live.com>
2016-07-04 23:46:52 -0500

    Hacer make en el benchmark
    al correr benchmark.js automaticamente se llama a make y se
    genera una nueva lista antes de realizar todos los tests.

--- benchmark.js
+++ benchmark.js
@@ -1,7 +1,8 @@
 const spawn = require('child_process').spawn;
 //const executable = process.argv[2]
 let index = 0
-const tests = ["countsort", "quicksort", "qsort", "insertionsort", "shakersort"]
+const tests = ["countsort", "quicksort", "qsort_gcc", "bitonicsort",
+"insertionsort", "shakersort"]
 
 function runTest(){
   const algorithmName = tests[index]
@@ -17,11 +18,36 @@ function runTest(){
   algoritmo.on('close', (code) => {
     const endTime = new Date().getTime()
     const diff = endTime - startTime
-    console.log(algorithmName + " tardo: " + diff + " ms.")
+    console.log(algorithmName + ":\t" + diff + " ms.")
     index++;
     if(index < tests.length)
       runTest()
   })
 }
 
-runTest();
+function createNewList(){
+  const listGenerator = spawn("./newlist", []);
+  listGenerator.stderr.setEncoding("utf8");
+  listGenerator.stderr.on('data', logOutput)
+  listGenerator.on('close', (code) => {
+    if(code == 0)
+      runTest()
+  })
+
+}
+function logOutput(data){
+  console.log(data)
+}
+function start(){
+  const make = spawn("make", []);
+  make.stdout.setEncoding('utf8');
+  make.stderr.setEncoding('utf8');
+  make.stdout.on('data', logOutput)
+  make.stderr.on('data', logOutput)
+  make.on('close', (code) => {
+    if(code == 0)
+      createNewList()
+  })
+}
+
+start();
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-all: newlist countsort quicksort insertionsort qsort shakersort
+all: newlist countsort quicksort insertionsort qsort_gcc shakersort bitonicsort
 
 newlist: FileGenerator.c
 	gcc FileGenerator.c -o newlist -Wall
@@ -12,14 +12,17 @@ quicksort: AlUtils.o quicksort.c
 insertionsort: AlUtils.o insertionsort.c
 	gcc AlUtils.o insertionsort.c -o insertionsort -Wall
 
-qsort: AlUtils.o qsort.c
-	gcc AlUtils.o qsort.c -o qsort -Wall
+qsort_gcc: AlUtils.o qsort.c
+	gcc AlUtils.o qsort.c -o qsort_gcc -Wall
 
 shakersort: AlUtils.o shakersort.c
 	gcc AlUtils.o shakersort.c -o shakersort -Wall
+
+bitonicsort: AlUtils.o bitonicsort.c
+	gcc AlUtils.o bitonicsort.c -o bitonicsort -Wall
 
 AlUtils.o: AlUtils.c
 	gcc -c AlUtils.c -o AlUtils.o
 
 clean:
-	rm *o nums.txt newlist countsort quicksort insertionsort
+	rm *o nums.txt newlist countsort quicksort insertionsort qsort_gcc shakersort bitonicsort
