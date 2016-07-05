2ed4e8975d9df72e5d37077eed6cfa0f86c54dda
Gabriel Aumala <gabrielfima@live.com>
2016-07-05 00:55:58 -0500

    Calcular promedio de cada test

--- benchmark.js
+++ benchmark.js
@@ -1,8 +1,36 @@
 const spawn = require('child_process').spawn;
-//const executable = process.argv[2]
-let index = 0
-const tests = ["countsort", "quicksort", "qsort_gcc", "bitonicsort",
+
+const silent = process.argv.length == 3 && process.argv[2] === "-s"
+const algoritmos = ["countsort", "quicksort", "qsort_gcc", "bitonicsort",
 "insertionsort", "shakersort"]
+const results = []
+const tests = []
+const iterations = 3 //cuantas veces voy a iterar para calcular promedio
+
+let index = 0
+
+function createRandomTests(){
+  for(let i = 0; i < iterations; i++){
+    const algoritmosCopy = Object.assign([], algoritmos)
+    while(algoritmosCopy.length > 0){
+      randomIndex = Math.random() * algoritmosCopy
+      deletedItem = algoritmosCopy.splice(randomIndex, 1)[0]
+      tests.push(deletedItem)
+    }
+  }
+}
+
+function initResults(){
+  algoritmos.forEach((al) => {
+    results[al] = 0
+  })
+}
+
+function postResults(){
+  algoritmos.forEach((al) => {
+    console.log(`${al},${parseFloat(results[al]/iterations).toFixed(3)} ms.`)
+  })
+}
 
 function runTest(){
   const algorithmName = tests[index]
@@ -18,10 +46,18 @@ function runTest(){
   algoritmo.on('close', (code) => {
     const endTime = new Date().getTime()
     const diff = endTime - startTime
-    console.log(algorithmName + ":\t" + diff + " ms.")
+    results[algorithmName] += diff
+    //console.log(algorithmName + ":\t" + diff + " ms.")
     index++;
-    if(index < tests.length)
-      runTest()
+    if(index < tests.length){
+      if((index % algoritmos.length === 0)){
+        createNewList()
+      } else {
+        runTest()
+      }
+    } else {
+      postResults();
+    }
   })
 }
 
@@ -36,7 +72,8 @@ function createNewList(){
 
 }
 function logOutput(data){
-  console.log(data)
+  if(!silent)
+    console.log(data)
 }
 function start(){
   const make = spawn("make", []);
@@ -50,4 +87,6 @@ function start(){
   })
 }
 
+initResults();
+createRandomTests();
 start();
